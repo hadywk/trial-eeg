92b59d9ca326bd9da8268583f0c41c0c3fa27292
Ahmed Sameh <94136734+Ghourab@users.noreply.github.com>
2023-04-16 21:14:32 +0200

    UI fixes

--- Mobile app/lib/screens/form_screen.dart
+++ Mobile app/lib/screens/form_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:gender_selection/gender_selection.dart';
+import 'package:gender_picker/source/enums.dart';
+import 'package:gender_picker/source/gender_picker.dart';
 
 import 'home_screen.dart';
-
 
 class FormScreen extends StatefulWidget {
   const FormScreen({Key? key}) : super(key: key);
@@ -20,7 +20,7 @@ class _MyHomePageState extends State<FormScreen> {
   TextEditingController address = TextEditingController();
   TextEditingController pincode = TextEditingController();
   Gender? finalGender;
-   void homeScreen(BuildContext ctx) {
+  void homeScreen(BuildContext ctx) {
     Navigator.of(ctx).push(
       MaterialPageRoute(
         builder: (_) {
@@ -31,104 +31,115 @@ class _MyHomePageState extends State<FormScreen> {
   }
 
   List<Step> stepList() => [
-    Step(
-
-      state: _activeStepIndex <= 0 ? StepState.editing : StepState.complete,
-      isActive: _activeStepIndex >= 0,
-      title: const Text('Account Details', style: TextStyle(fontFamily: 'Bebas', letterSpacing: 2),),
-      content: Container(
-        child: Column(
-          children: [
-            TextField(
-              controller: name,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Full Name',
-              ),
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            TextField(
-              controller: email,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Birthdate',
-              ),
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            TextField(
-              controller: pass,
-              obscureText: true,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Age',
-              ),
-            ),
-            GenderSelection(
-maleText: "", //default Male
-femaleText: "", //default Female
-
-selectedGenderIconBackgroundColor: Colors.indigo, // default red 
-checkIconAlignment: Alignment.centerRight,   // default bottomRight
-selectedGenderCheckIcon: null, // default Icons.check
-onChanged: (Gender gender){
-  finalGender=gender;
-print(gender);
-},
-equallyAligned: true,
-animationDuration: Duration(milliseconds: 400),
-isCircular: true, // default : true,
-isSelectedGenderIconCircular: true,
-opacityOfGradient: 0.6,
-padding: const EdgeInsets.all(3),
-size: 120, //default : 120
-
-),
-          ],
-        ),
-      ),
-    ),
-    
-    Step(
-        state:
-        _activeStepIndex <= 1 ? StepState.editing : StepState.complete,
-        isActive: _activeStepIndex >= 1,
-        title: const Text('Address', style: TextStyle(fontFamily: 'Bebas', letterSpacing: 2,),),
-        content: Container(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 8,
-              ),
-              TextField(
-                controller: address,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Present Address',
-                ),
-              ),
-              const SizedBox(
-                height: 8,
-              ),
-              TextField(
-                controller: pincode,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Permanent Address',
-                ),
-              ),
-            ],
+        Step(
+          state: _activeStepIndex <= 0 ? StepState.editing : StepState.complete,
+          isActive: _activeStepIndex >= 0,
+          title: const Text(
+            'Account Details',
+            style: TextStyle(fontFamily: 'Bebas', letterSpacing: 2),
           ),
-        )),
-    Step(
-        state: StepState.complete,
-        isActive: _activeStepIndex >= 2,
-        title: const Text('Confirm Details', style: TextStyle(fontFamily: 'Bebas', letterSpacing: 2,),),
-        content: Container(
+          content: Container(
             child: Column(
+              children: [
+                TextField(
+                  controller: name,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Full Name',
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                TextField(
+                  controller: email,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Birthdate',
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                TextField(
+                  controller: pass,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Age',
+                  ),
+                ),
+                GenderPickerWithImage(
+                  verticalAlignedText: false,
+                  selectedGender: Gender.Male,
+                  selectedGenderTextStyle: TextStyle(
+                      color: Color(0xFF8b32a8), fontWeight: FontWeight.bold),
+                  unSelectedGenderTextStyle: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.normal),
+                  onChanged: (Gender? gender) {
+                    print(gender);
+                  },
+                  equallyAligned: true,
+                  animationDuration: Duration(milliseconds: 300),
+                  isCircular: true,
+                  // default : true,
+                  opacityOfGradient: 0.4,
+                  padding: const EdgeInsets.all(3),
+                  size: 50, //default : 40
+                ),
+              ],
+            ),
+          ),
+        ),
+        Step(
+            state:
+                _activeStepIndex <= 1 ? StepState.editing : StepState.complete,
+            isActive: _activeStepIndex >= 1,
+            title: const Text(
+              'Address',
+              style: TextStyle(
+                fontFamily: 'Bebas',
+                letterSpacing: 2,
+              ),
+            ),
+            content: Container(
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  TextField(
+                    controller: address,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Present Address',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  TextField(
+                    controller: pincode,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Permanent Address',
+                    ),
+                  ),
+                ],
+              ),
+            )),
+        Step(
+            state: StepState.complete,
+            isActive: _activeStepIndex >= 2,
+            title: const Text(
+              'Confirm Details',
+              style: TextStyle(
+                fontFamily: 'Bebas',
+                letterSpacing: 2,
+              ),
+            ),
+            content: Container(
+                child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
@@ -140,47 +151,51 @@ size: 120, //default : 120
                 Text('Gender : ${finalGender}'),
               ],
             )))
-  ];
+      ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Information', style: TextStyle(fontFamily: 'Bebas', letterSpacing: 1,),
+        title: const Text(
+          'Information',
+          style: TextStyle(
+            fontFamily: 'Bebas',
+            letterSpacing: 1,
+          ),
         ),
         centerTitle: true,
         backgroundColor: Colors.pink,
       ),
       body: Stepper(
-        type: StepperType.vertical,
-        currentStep: _activeStepIndex,
-        steps: stepList(),
-        onStepContinue: () {
-          if (_activeStepIndex < (stepList().length - 1)) {
-            setState(() {
-              _activeStepIndex += 1;
-            });
-          } else {
-            print('Submited');
+          type: StepperType.vertical,
+          currentStep: _activeStepIndex,
+          steps: stepList(),
+          onStepContinue: () {
+            if (_activeStepIndex < (stepList().length - 1)) {
+              setState(() {
+                _activeStepIndex += 1;
+              });
+            } else {
+              print('Submited');
               homeScreen(context);
-          }
-        },
-        onStepCancel: () {
-          if (_activeStepIndex == 0) {
-            return;
-          }
+            }
+          },
+          onStepCancel: () {
+            if (_activeStepIndex == 0) {
+              return;
+            }
 
-          setState(() {
-            _activeStepIndex -= 1;
-          });
-        },
-        onStepTapped: (int index) {
-          setState(() {
-            _activeStepIndex = index;
-          });
-        },
-        controlsBuilder: null
-      ),
+            setState(() {
+              _activeStepIndex -= 1;
+            });
+          },
+          onStepTapped: (int index) {
+            setState(() {
+              _activeStepIndex = index;
+            });
+          },
+          controlsBuilder: null),
     );
   }
 }
