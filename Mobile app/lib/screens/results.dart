8fbe377b43325b95eff8235679d79f33dfb15368
Ahmed Sameh <94136734+Ghourab@users.noreply.github.com>
2023-05-12 15:02:21 +0300

    push notification first implementation

--- Mobile app/lib/screens/results.dart
+++ Mobile app/lib/screens/results.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'feedback_screen.dart';
 import 'package:graduation/screens/processing_signals_screen.dart';
@@ -10,6 +12,20 @@ class Results extends StatefulWidget {
 }
 
 class _ResultsState extends State<Results> {
+  void setupPushNotification() async {
+    final fcm = FirebaseMessaging.instance;
+    //final notificationSettings=await fcm.requestPermission();
+
+    final token = await fcm.getToken();
+    print(token);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotification();
+  }
+
   @override
   Widget build(BuildContext context) {
     var x = 'food';
